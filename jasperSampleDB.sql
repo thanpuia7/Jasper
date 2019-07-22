3ffaa3dd3546afb48d0a6167126b67286d1596f5
Lalthanpuia <30722010+thanpuia7@users.noreply.github.com>
2019-07-22 15:30:41 +0530

    Database structure with sample data

---
+++ jasperSampleDB.sql
@@ -0,0 +1,77 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.7.26-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `emp_master` (
+	`emp_code` varchar ,
+	`EmpName` varchar ,
+	`Salary` Decimal ,
+	`Doj` date 
+); 
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5000','Kumar','4500000','2000-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5001','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
+insert into `emp_master` (`emp_code`, `EmpName`, `Salary`, `Doj`) values('5002','Jacob','4000000','2002-01-01');
